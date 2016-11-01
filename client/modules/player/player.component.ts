0eecd15f8b88c557650c0cb87aaf4778831de548
Wilson Hamilton <wilsonhamiltond@gmail.com>
2016-11-01 17:25:34 +0000

    adding loading image

--- client/modules/player/player.component.ts
+++ client/modules/player/player.component.ts
@@ -5,7 +5,19 @@ import { Sound } from '../../interfaces/player/sound.interface';
 
 @Component({
     selector: 'player',
-    template: ``,
+    styles: [ `
+        .player{
+            position:absolute;
+            z-index: 1000;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+        }
+    `],
+    template: `
+    <div class="col-lg-12 no-padding-l-r player">
+        <h1>Hola mundo</h1>
+    </div>`,
     providers: [PlayerService]
 })
 export class PlayerComponent{
