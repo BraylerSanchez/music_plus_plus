fdbf826990cfc65a9e9cbb62f0783dc53461f5c6
Wilson A. Hamilton <wilsonhamiltond@gmail.com>
2016-11-01 22:00:53 -0400

    fixing

--- client/modules/home/home.routes.js
+++ client/modules/home/home.routes.js
@@ -1,7 +1,7 @@
 "use strict";
 var router_1 = require('@angular/router');
-var home_component_1 = require('./home.component');
-var search_component_1 = require('./search.component');
+var home_component_1 = require('./components/home.component');
+var search_component_1 = require('./components/search.component');
 exports.routes = [
     {
         path: 'home',
